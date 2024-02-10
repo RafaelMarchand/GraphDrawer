c2e8e7b44257c9e40549c41f38714daab5dbc221
RafaelMarchand <rafaelm_1@live.de>
2024-02-10 22:43:28 +0100

    refactor graph pop ups, optimized hide delay for remove button

--- src/GraphConverter.js
+++ src/GraphConverter.js
@@ -2,36 +2,37 @@ import CanvasEdge from "./CanvasEdge.js"
 import CanvasNode from "./CanvasNode.js"
 
 export default function graphConverter(graph, graphMethods, rootNodeKeys) {
-    let canvasNodes = []
-    let canvasRootNodes = []
-    let nodeValue = null
-    let nodeFocus = null
+  let canvasNodes = []
+  let canvasRootNodes = []
+  let nodeValue = null
+  let nodeFocus = null
 
-    graphMethods.getNodeKeys(graph).forEach((nodeKey) => {
-        if (graphMethods.getNodeValue && graphMethods.getNodeValue != null) {
-            nodeValue = graphMethods.getNodeValue(graph, nodeKey)
-        }
-        // get the node key(s) for the focused node(s)
-        if (graphMethods.getNodeFocus && graphMethods.getNodeFocus != null) {
-            nodeFocus = graphMethods.getNodeFocus(graph, nodeKey)
-        }
-        canvasNodes.push(new CanvasNode(nodeKey, graphMethods.getOutEdgesKeys(graph, nodeKey), nodeValue, nodeFocus))
-    })
+  graphMethods.getNodeKeys(graph).forEach((nodeKey) => {
+    if (graphMethods.getNodeValue && graphMethods.getNodeValue != null) {
+      nodeValue = graphMethods.getNodeValue(graph, nodeKey)
+    }
+    // get the node key(s) for the focused node(s)
+    if (graphMethods.getNodeFocus && graphMethods.getNodeFocus != null) {
+      nodeFocus = graphMethods.getNodeFocus(graph, nodeKey)
+    }
+    canvasNodes.push(
+      new CanvasNode(nodeKey, graphMethods.getOutEdgesKeys(graph, nodeKey), nodeValue, nodeFocus)
+    )
+  })
 
-    canvasNodes.forEach(canvasNode => {
-        canvasNode.edges = canvasNode.edges.map(edgeKey => {
-            
-            return new CanvasEdge(canvasNode,
-                canvasNodes.find(cNode => {
-                    return cNode.name === graphMethods.getDestNodeKey(graph, edgeKey)
-                }))
+  canvasNodes.forEach((canvasNode) => {
+    canvasNode.edges = canvasNode.edges.map((edgeKey) => {
+      return new CanvasEdge(
+        canvasNode,
+        canvasNodes.find((cNode) => {
+          return cNode.name === graphMethods.getDestNodeKey(graph, edgeKey)
         })
+      )
     })
+  })
 
-    rootNodeKeys.forEach(nodeKey => {
-        canvasRootNodes.push(canvasNodes.find((canvasNode => canvasNode.name === nodeKey)))
-    })
-    return [canvasNodes, canvasRootNodes]
+  rootNodeKeys.forEach((nodeKey) => {
+    canvasRootNodes.push(canvasNodes.find((canvasNode) => canvasNode.name === nodeKey))
+  })
+  return [canvasNodes, canvasRootNodes]
 }
-
-
